c4c812845afea64e4704a788caf4c6efdd0f3ff4
Piyush Gupta <116789485+Piyuzh77@users.noreply.github.com>
2024-11-03 01:54:32 +0530

    Update Card.jsx

--- src/Components/Cards/Card.jsx
+++ src/Components/Cards/Card.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 const Card = ({ items, theme }) => {
-    const isDark= theme==='dark';
+  const isDark = theme === "dark";
   return (
     <>
-      <div className={`md:w-[380px] w-[400px] sm:w-[300px] h-auto rounded-lg ${isDark?"bg-darkNav text-light ":"bg-light text-darkNav"}   flex p-2 px-6 items-center hover:rotate-1`}>
+      <div
+        className={`md:max-w-[380px] w-full sm:max-w-[300px] h-auto rounded-lg ${
+          isDark ? "bg-darkNav text-light" : "bg-light text-darkNav"
+        } flex p-2 px-6 items-center hover:rotate-1`}
+      >
         <img
           src={items.src}
           className="h-12 w-12 flex-shrink-0 border drop-shadow-lg shadow-2xl rounded-full bg-lightBlue"
@@ -12,9 +16,7 @@ const Card = ({ items, theme }) => {
         />
         <div className="ml-2 px-4 hover:-rotate-1">
           <h1 className="font-bold">{items.name}</h1>
-          <p className="text-sm">
-            {items.description}
-          </p>
+          <p className="text-sm">{items.description}</p>
         </div>
       </div>
     </>
